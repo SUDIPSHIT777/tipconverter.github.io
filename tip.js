116f8e6bceee306eda578b3e13434acf36962462
SUDIPSHIT777 <sudipshit777@gmail.com>
2025-08-16 09:04:34 +0530

    tip.js

--- tip.js
+++ tip.js
@@ -1,10 +1,13 @@
 let convert = document.getElementById("convert");
 let result = document.getElementById("result");
 convert.addEventListener("click", () => {
-    let input1 = document.getElementById("in1").value;
-    let input2 = document.getElementById("in2").value;
+    let input1 = parseInt(document.getElementById("in1").value);
+    let input2 = parseInt(document.getElementById("in2").value);
     if (input1 === "" && input2 === "") {
         result.textContent = "Give Two Input";
+    }
+    else if (input1 <= input2) {
+        result.textContent = "Price is Greter Then Tip";
     }
     else {
         let output = (input1 * input2) / 100;
